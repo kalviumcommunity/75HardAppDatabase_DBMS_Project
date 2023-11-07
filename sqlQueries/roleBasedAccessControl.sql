7136c602e9473911771aac0a30abecaccda442f1
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-11-07 07:09:52 +0530

    Added create if not exists query

--- sqlQueries/roleBasedAccessControl.sql
+++ sqlQueries/roleBasedAccessControl.sql
@@ -1,26 +1,20 @@
--- Create Roles
-CREATE ROLE Administrator;
-CREATE ROLE AppUser;
+USE 75HardDatabase;
 
--- Create User 'admin' with Password 'admin_password' and Assign the Administrator Role
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin_password';
-GRANT Administrator TO 'admin'@'localhost';
+-- Create Users 'Administrator' and 'AppUser'
+CREATE USER IF NOT EXISTS 'Administrator'@'localhost' IDENTIFIED BY 'admin_password';
+CREATE USER IF NOT EXISTS 'AppUser'@'localhost' IDENTIFIED BY 'appuser_password';
 
--- Create User 'appuser' with Password 'appuser_password' and Assign the AppUser Role
-CREATE USER 'appuser'@'localhost' IDENTIFIED BY 'appuser_password';
-GRANT AppUser TO 'appuser'@'localhost';
+-- Grant All Privileges on the Entire Database to 'Administrator'
+GRANT ALL PRIVILEGES ON 75HardDatabase.* TO 'Administrator'@'localhost';
 
--- Grant All Privileges to Administrator
-GRANT ALL PRIVILEGES ON 75HardDatabase.* TO Administrator;
+-- Grant SELECT Privileges on the Entire Database to Both 'Administrator' and 'AppUser'
+GRANT SELECT ON 75HardDatabase.* TO 'Administrator'@'localhost', 'AppUser'@'localhost';
 
--- Grant Select Privileges to Both Roles
-GRANT SELECT ON ALL TABLES IN 75HardDatabase TO Administrator, AppUser;
+-- Revoke Insert, Update, Delete Privileges from 'AppUser'
+REVOKE INSERT, UPDATE, DELETE ON 75HardDatabase.* FROM 'AppUser'@'localhost';
 
--- Revoke Insert, Update, Delete Privileges from AppUser
-REVOKE INSERT, UPDATE, DELETE ON ALL TABLES IN 75HardDatabase FROM AppUser;
+-- Grant Update Privileges on Users Table to 'AppUser'
+GRANT UPDATE (Username, Password) ON 75HardDatabase.users TO 'AppUser'@'localhost';
 
--- Grant Update Privileges on Users Table to AppUser
-GRANT UPDATE (Username, Password) ON 75HardDatabase.users TO AppUser;
-
--- Grant Update Privileges on Challenge Logs Table to AppUser
-GRANT UPDATE (ChallengeStatus) ON 75HardDatabase.challenge_logs TO AppUser;
+-- Grant Update Privileges on Challenge Logs Table to 'AppUser'
+GRANT UPDATE (ChallengeStatus) ON 75HardDatabase.challenge_logs TO 'AppUser'@'localhost';
